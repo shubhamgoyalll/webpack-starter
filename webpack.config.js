a12bf89af2efefa2f662049c5b01b590c14e67ee
Shubham Goyal <goyalshubham2521@gmail.com>
2024-09-03 16:39:28 +0530

    clean - it creates only one js bundle in dist and source map -  show the error in source code

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,13 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
+    // It takes name from entry's (bundle) filename and then add hashname to it
     filename: "[name][contenthash].js",
+    //so that it creates only one js bundle in dist
+    clean: true,
   },
+  //will show the error in source code from production code now
+  devtool: "source-map",
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
